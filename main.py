68ebc3964c6578e84a85a960036766d1698e5f19
Lan Nguyen <ydf512003@gmail.com>
2025-06-28 23:55:46 +0700

    New features: Self-test, paging by 100 questions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import json
+import random
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -15,4 +16,12 @@ def index(request: Request):
     return templates.TemplateResponse("index.html", {
         "request": request,
         "questions": questions
+    })
+
+@app.get("/exam")
+def exam(request: Request):
+    sample = random.sample(questions, 40)
+    return templates.TemplateResponse("exam.html", {
+        "request": request,
+        "questions": sample
     })
